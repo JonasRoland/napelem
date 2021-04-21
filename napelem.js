42605e21a39fe7202726928334084d5be563e4e8
JonasRoland <jonasroland49@gmail.com>
2021-04-21 09:32:55 +0200

    7-8. feladat

--- napelem.js
+++ napelem.js
@@ -4,6 +4,8 @@ function panelek(db){
     for (i = 0; i < db; i++) {
         panelek.innerHTML += "<img src='napelem_ikon.png'>"; 
     }
+    panelek.innerHTML+="<br>";
     panelek.innerHTML += "("+db+" db)";
     document.getElementById('osszteljesitmeny').value = db * 275;
+    document.getElementById('tetofelulet').value = db * 3.5;
 }
